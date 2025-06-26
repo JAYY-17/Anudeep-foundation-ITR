45c511e3c8046aa0591557c0e9977c1b045ccb97
JAYY-17 <patiljayvardhan17@gmail.com>
2025-06-26 10:16:14 +0530

    Add files via upload

---
+++ pattern.java
@@ -0,0 +1,36 @@
+public class pattern
+{
+  public static void main(String[] args) 
+  {
+    int rows= 4;
+
+     /*  for(int i=1;i<=rows;i++)
+     {
+        for(int j=1;j<=i;j++)
+        {
+          System.out.print("*");
+        }  
+        System.out.println();
+     }
+     *
+     **
+     ***
+     ****  
+*/
+   
+    int cols=4;
+    for(int i=1;i<=rows;i++)
+    {
+        for(int j=1;j<=cols;j++)
+        {
+         System.out.print("*");
+        }
+        System.out.println();
+    }
+/*  ****
+    ****
+    ****
+    ****
+    */
+  } 
+}
